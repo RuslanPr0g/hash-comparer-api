31f76a74893f2a72b429a94ed376d3ee20bd9a84
NB-RUDS\Administrator <ruds@opinov8.com>
2021-07-30 13:06:10 +0300

    private set for global variables

--- HashComparer/HashComparer/Shared/GlobalVariables.cs
+++ HashComparer/HashComparer/Shared/GlobalVariables.cs
@@ -2,8 +2,8 @@
 {
     public static class GlobalVariables
     {
-        public static string HashingPath { get; set; } = "Hashing";
-        public static string SpecialKeyPath { get; set; } = "Hashing:SpecialKey";
-        public static string KeyIdPath { get; set; } = "Hashing:KeyId";
+        public static string HashingPath { get; private set; } = "Hashing";
+        public static string SpecialKeyPath { get; private set; } = "Hashing:SpecialKey";
+        public static string KeyIdPath { get; private set; } = "Hashing:KeyId";
     }
 }
